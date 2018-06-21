e88f38a95d67c9f430d6470ab4579b7a89253b57
Bartosz Jedrzejewski <jedrus07@gmail.com>
2018-06-21 23:53:26 +0100

    Getting all the files and endpoints configured

--- src/main/java/com/e4developer/functionalciaworldfactbook/FunctionalCiaWorldFactbookApplication.java
+++ src/main/java/com/e4developer/functionalciaworldfactbook/FunctionalCiaWorldFactbookApplication.java
@@ -2,24 +2,22 @@ package com.e4developer.functionalciaworldfactbook;
 
 import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
-import org.springframework.web.reactive.function.server.HandlerFunction;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Mono;
 import reactor.ipc.netty.http.server.HttpServer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 public class FunctionalCiaWorldFactbookApplication {
 
-    public static void main(String[] args) throws InterruptedException {
-        HandlerFunction hello = request -> ServerResponse.ok().body(Mono.just("Hello"), String.class);
-
-        RouterFunction router = route(GET("/"), hello);
-
-        HttpHandler httpHandler = RouterFunctions.toHttpHandler(router);
+    public static void main(String[] args) throws InterruptedException, FileNotFoundException {
+        HttpHandler httpHandler = RouterFunctions.toHttpHandler(createRouterFunction());
 
         HttpServer
                 .create("localhost", 8080)
@@ -28,4 +26,40 @@ public class FunctionalCiaWorldFactbookApplication {
 
         Thread.currentThread().join();
     }
+
+    private static RouterFunction createRouterFunction() throws FileNotFoundException {
+        RouterFunction<ServerResponse> routerFunction
+                = route(GET("/"),
+                request -> createStringResponse("Welcome to the CIA World Factbook. Check for directories with /directories"));
+        routerFunction = routeWithDirectories(routerFunction);
+
+        return routerFunction;
+    }
+
+    private static RouterFunction<ServerResponse> routeWithDirectories(RouterFunction<ServerResponse> routerFunction) throws FileNotFoundException {
+        File file = ResourceUtils.getFile("classpath:factbook");
+        String[] directories = file.list();
+        routerFunction = routerFunction.andRoute(GET("/directories"),
+                request -> createStringResponse(String.join(",", directories)));
+        for(String directory : directories){
+            File countriesDir = ResourceUtils.getFile("classpath:factbook/"+directory);
+            String[] countries = countriesDir.list();
+            routerFunction = routerFunction.andRoute(GET("/"+directory),
+                    request -> createStringResponse(String.join(",", countries)));
+            for(String country : countries){
+                File countryFile = ResourceUtils.getFile("classpath:factbook/"+directory+"/"+country);
+                Scanner scanner = new Scanner(countryFile, "UTF-8" );
+                String fileText = scanner.useDelimiter("\\A").next();
+                scanner.close();
+                routerFunction = routerFunction.andRoute(GET("/"+directory+"/"+country),
+                        request -> createStringResponse(fileText));
+            }
+        }
+        return routerFunction;
+    }
+
+    private static Mono<ServerResponse> createStringResponse(String response){
+        return ServerResponse.ok().body(Mono.just(response), String.class);
+    }
+
 }
